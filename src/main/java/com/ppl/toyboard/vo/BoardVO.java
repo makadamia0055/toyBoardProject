32a61bccf8afc697530cc5791bbbc2615c0b30ed
makadamia0055 <makend91@naver.com>
2023-08-18 19:09:26 +0900

    add jpa setting and mapping boardVO

---
+++ src/main/java/com/ppl/toyboard/vo/BoardVO.java
@@ -0,0 +1,26 @@
+package com.ppl.toyboard.vo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="bo_board")
+public class BoardVO {
+	@Id
+	@GeneratedValue
+	private int bo_num;
+	private String bo_title;
+	private String bo_content;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date bo_date;
+	private String bo_us_id;
+	private int bo_ct_num;
+	private int bo_state;
+	
+}
