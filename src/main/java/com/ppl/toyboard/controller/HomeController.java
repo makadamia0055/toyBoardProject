13fa00c6af0c386b3acbf842a268c684aaa5a8a9
makadamia0055 <makend91@naver.com>
2023-08-20 17:13:04 +0900

    check jpa connection

--- src/main/java/com/ppl/toyboard/controller/HomeController.java
+++ src/main/java/com/ppl/toyboard/controller/HomeController.java
@@ -1,34 +1,64 @@
 package com.ppl.toyboard.controller;
 
-import java.text.DateFormat;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ppl.toyboard.root.vo.BoardVO;
 
 
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	EntityManagerFactory emf;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView home(ModelAndView mv) {
+
+        
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            // 게시글 추가
+            BoardVO board = new BoardVO();
+            board.setBo_title("JPA 제목");
+            board.setBo_us_id("관리자");
+            board.setBo_content("JPA 글 내용 테스트");
+            board.setBo_date(new Date());
+
+            em.persist(board);
+
+            tx.commit();
+
+            // 글 목록 조회
+            String jpql = "select b from BoardVO b order by b.bo_num desc";
+            List<BoardVO> boardList = em.createQuery(jpql, BoardVO.class).getResultList();
+            
+            mv.addObject("boardList", boardList);
+
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+        mv.setViewName("home");
+		return mv;
 	}
 	
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,9 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <html>
 <head>
+    <meta charset="UTF-8">
+
 	<title>Home</title>
 </head>
 <body>
@@ -10,5 +12,13 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<c:forEach var="board" items="${boardList}">
+            <tr>
+                <td>${board.bo_num}</td>
+                <td>${board.bo_title}</td>
+                <td>${board.bo_us_id}</td>
+                <!-- 필요한 컬럼들 출력 -->
+            </tr>
+        </c:forEach>
 </body>
 </html>
