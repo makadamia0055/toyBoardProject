f1e7ae66aeb039c9880e83c321baba7d3d1cad32
makadamia0055 <makend91@naver.com>
2023-09-01 17:19:43 +0900

    add auth controller and service imp

---
+++ src/main/java/com/ppl/toyboard/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.ppl.toyboard.controller;
+
+public class AuthController {
+
+}
---
+++ src/main/java/com/ppl/toyboard/root/service/AuthServiceImp.java
@@ -0,0 +1,8 @@
+package com.ppl.toyboard.root.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImp implements AuthService{
+
+}
