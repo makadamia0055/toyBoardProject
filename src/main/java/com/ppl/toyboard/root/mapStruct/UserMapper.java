e9eba184319914c3fc55851b99c49ff2916a935a
makadamia0055 <makend91@naver.com>
2023-09-02 18:25:44 +0900

    mapper test(not working)

---
+++ src/main/java/com/ppl/toyboard/root/mapStruct/UserMapper.java
@@ -0,0 +1,23 @@
+package com.ppl.toyboard.root.mapStruct;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.ppl.toyboard.root.dto.LoginUserDTO;
+import com.ppl.toyboard.root.dto.RegisterUserDTO;
+import com.ppl.toyboard.root.entity.User;
+import com.ppl.toyboard.root.vo.UserVO;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+	
+	User toEntity(LoginUserDTO userDTO);
+	
+	/*
+	 * User toEntity(RegisterUserDTO userDTO);
+	 * 
+	 * User toEntity(UserVO userVO);
+	 */
+	
+}
---
+++ src/test/java/com/ppl/toyboard/root/common/MapStructTest.java
@@ -0,0 +1,31 @@
+package com.ppl.toyboard.root.common;
+
+import javax.persistence.Entity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.ppl.toyboard.root.dto.LoginUserDTO;
+import com.ppl.toyboard.root.entity.User;
+import com.ppl.toyboard.root.mapStruct.UserMapper;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations= "classpath:test-Business-layer.xml")
+public class MapStructTest {
+	
+	
+	@Test
+	public void mapLoginDTOToEntity() {
+		LoginUserDTO loginUserDto = new LoginUserDTO();
+		loginUserDto.setUs_id("mak123");
+		loginUserDto.setUs_pw("mak123Pw");
+		
+		User entityUser = UserMapper.INSTANCE.toEntity(loginUserDto);
+		System.out.println(entityUser.getUs_id());
+		System.out.println(entityUser.getUs_pw());
+		System.out.println(entityUser instanceof Entity);
+	}
+	
+}
